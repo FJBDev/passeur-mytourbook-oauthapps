aff75317bddf33b88830d607b31f5258813ddf72
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-01-26 00:31:59 +0000

    WIP

--- server.js
+++ server.js
@@ -197,7 +197,6 @@ async function retrieveStravaToken(grantType, code, refreshToken) {
     console.error('Access Token Error', error.message);
   }
 }
-export { retrieveStravaToken };
 app.get("/openweathermap/timemachine", async (request, response) => {
 
   if (!request.query.units || request.query.units !== 'metric') {
@@ -276,3 +275,4 @@ app.get("/weatherapi", async (request, response) => {
       }
     });
 })
+module.exports = { app }
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,22 +1,43 @@
-//import { retrieveStravaToken } from '../server.js'
+import { app } from '../server.js'
+const supertest = require('supertest');
+const requestWithSupertest = supertest(app);
 
-// This is just for organisation and reporting
-describe('Our application', function () {
+describe('User Endpoints', () => {
 
-  // This is the name of the test
-  it('should understand basic mathematical principles', function (done) {
-
-    expect(1==1).toBe(true);
-    expect(1==2).not.toBe(true);
-    // We want this test to pass.
-    if (5 == 5) {
-      // If the behavior is as expected, call done with no argument.
-      done();
-    } else {
-      // Otherwise, call done with an error.
-      done(new Error("Not sure what's happened."));
-    }
-
+  it('GET /user should show all users', async () => {
+    const res = await requestWithSupertest.get();//'weatherapi?lat=40.263996&lon=-105.58854099999999&dt=2023-01-24&lang=fr');
+    expect(res.status).toEqual(200);
+    //expect(res.type).toEqual(expect.stringContaining('json'));
+    //expect(res.body).toHaveProperty('users')
   });
 
 });
+
+/* describe('OpenWeatherMap Weather Retrieval', function () {
+
+  test("Should return 200", async () => {
+
+    //expect.hasAssertions();
+
+    const stravaToken = await server.retrieveStravaToken("", "", "");
+
+    expect(stravaToken).toEqual("");
+  })
+
+  // This is the name of the test
+    it('should understand basic mathematical principles', function (done) {
+ 
+     expect(1 == 1).toBe(true);
+     expect(1 == 2).not.toBe(true);
+     // We want this test to pass.
+     if (5 == 5) {
+       // If the behavior is as expected, call done with no argument.
+       done();
+     } else {
+       // Otherwise, call done with an error.
+       done(new Error("Not sure what's happened."));
+     }
+ 
+   }); 
+
+}); */
