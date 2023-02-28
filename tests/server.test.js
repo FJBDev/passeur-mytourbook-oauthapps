6c129bf44e8ed1c4c59a23a54015781fa1897243
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-02-28 16:40:21 -0700

    [OpenWeatherMap] Adding the air quality index retrieval (#46)

--- tests/server.test.js
+++ tests/server.test.js
@@ -39,6 +39,24 @@ describe('OpenWeatherMap Weather Retrieval', () => {
 
 });
 
+describe('OpenWeatherMap Air Quality Retrieval', () => {
+
+  test('GET /openweathermap should return 200', async () => {
+
+    expect.hasAssertions();
+
+    var yesterdayDate = getYesterdaysDate();
+    var start = Math.floor(yesterdayDate.getTime() / 1000);
+    var end = start + 3600;
+
+    const res = await requestWithSupertest.get('/openweathermap/air_pollution?lat=40.26&lon=-105.58&start=' + start +
+      '&end=' + end);
+
+    expect(res.status).toEqual(200);
+  });
+
+});
+
 describe('WeatherApi Weather Retrieval', () => {
 
   test('GET /weatherapi should return 200', async () => {
