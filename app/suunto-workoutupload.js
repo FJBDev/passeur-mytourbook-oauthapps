083ea0d147cbf2d67c20eed0b23a6292eb1dd77a
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-04-04 16:59:44 +0000

    merge fix

---
+++ app/suunto-workoutupload.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+function initializeUpload(request, response) {
+
+    const { authorization } = request.headers;
+
+    var config = {
+        method: 'post',
+        url: 'https://cloudapi.suunto.com/v2/upload/',
+        headers: {
+            'Authorization': authorization,
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': process.env.SUUNTO_SUBSCRIPTION_KEY
+        },
+        data: request.body
+    };
+
+    axios(config)
+        .then(function (result) {
+            response.status(201).send(result.data);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                response.status(error.response.status).send(error.response.data.message);
+            } else {
+                response.status(400).send(error.message);
+            }
+        });
+}
+
+module.exports = initializeUpload;
