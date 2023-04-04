a39abd076f418bf88989a010aeddb61848759855
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-04-04 11:04:42 -0600

    [Suunto] Workout upload initialization fix (#50)

--- app/suunto-workoutupload.js
+++ app/suunto-workoutupload.js
@@ -11,16 +11,17 @@ function initializeUpload(request, response) {
             'Authorization': authorization,
             'Content-Type': 'application/json',
             'Ocp-Apim-Subscription-Key': process.env.SUUNTO_SUBSCRIPTION_KEY
-        }
+        },
+        data: request.body
     };
 
     axios(config)
         .then(function (result) {
-            response.status(201).send(result.data);
+            response.status(result.status).send(result.data);
         })
         .catch(function (error) {
             if (error.response) {
-                response.status(error.response.status).send(error.message);
+                response.status(error.response.status).send(error.response.data.message);
             } else {
                 response.status(400).send(error.message);
             }
