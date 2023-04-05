27ec636d3b0c538c6f7c9814ef99c1a9bd06813f
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-04-05 00:00:58 +0000

    WIP

---
+++ app/suunto-constants.js
@@ -0,0 +1,7 @@
+export const suuntoBaseUrl = 'https://cloudapi.suunto.com/v2';
+
+//TODO FB segregate the suunto route upload and file download to separate file
+
+// Segregate the strava endpoints
+
+// Unit tests for the workout import (using mock?)
