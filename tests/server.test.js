ad6a7a30fb8447af3269a8ceea9c637d222ffc12
FJBDev <25235665+FJBDev@users.noreply.github.com>
2024-05-24 17:35:39 +0000

    merge part 5

--- tests/server.test.js
+++ tests/server.test.js
@@ -81,43 +81,48 @@ describe('WeatherApi Weather Retrieval', () => {
 
     const res = await requestWithSupertest.get('/weatherapi?lat=40.26&lon=-105.58&dt=' + dt);
 
-    describe('Strava Token Retrieval', () => {
+    expect(res.status).toEqual(200);
+  });
 
-      test('POST /strava/token should return 201', async () => {
+});
 
-        //expect.hasAssertions();
-        // const spy = sinon.spy(capp, 'retrieveStravaToken');
-        // jest.spyOn(app, 'retrieveStravaToken').mockImplementation(() => {
-        //   return {
-        //     "hikes": [
-        //       {
-        //         "route_id": 123,
-        //         "title": "My Mocked Hike",
-        //         "stats": {
-        //           "startTime": "2019-10-30T16:49:40Z",
-        //           "endTime": "2019-10-30T19:05:28Z"
-        //         }
-        //       }
-        //     ]
-        //   }
-        // });
-        //jwtSpy.mockReturnValue('Some decoded token');
-        const res = await requestWithSupertest.post('/strava/token').send('{ "client_id":"client_id", "refresh_token":"refresh_token", "client_secret":"client_secret", "code":"code", "grant_type":"authorization_code" }');
-        //assert(spy.called);
-        console.log(res);
-        expect(res.status).toEqual(200);
-        // var req = http.request(options, function (res) {
-        //   expect(res.tatus).toEqual(200);
-        //   res.on('data', function (chunk) {
-        //     console.log('BODY: ' + chunk);
-        //   });
-        // });
+describe('Strava Token Retrieval', () => {
 
-        // req.on('error', function (e) {
-        //   console.log('error: ' + e);
-        // });
+  test('POST /strava/token should return 201', async () => {
 
-        //req.end();
-      });
+    //expect.hasAssertions();
+    // const spy = sinon.spy(capp, 'retrieveStravaToken');
+    // jest.spyOn(app, 'retrieveStravaToken').mockImplementation(() => {
+    //   return {
+    //     "hikes": [
+    //       {
+    //         "route_id": 123,
+    //         "title": "My Mocked Hike",
+    //         "stats": {
+    //           "startTime": "2019-10-30T16:49:40Z",
+    //           "endTime": "2019-10-30T19:05:28Z"
+    //         }
+    //       }
+    //     ]
+    //   }
+    // });
+    //jwtSpy.mockReturnValue('Some decoded token');
+    const res = await requestWithSupertest.post('/strava/token').send('{ "client_id":"client_id", "refresh_token":"refresh_token", "client_secret":"client_secret", "code":"code", "grant_type":"authorization_code" }');
+    //assert(spy.called);
+    console.log(res);
+    expect(res.status).toEqual(200);
+    // var req = http.request(options, function (res) {
+    //   expect(res.tatus).toEqual(200);
+    //   res.on('data', function (chunk) {
+    //     console.log('BODY: ' + chunk);
+    //   });
+    // });
 
-    });
+    // req.on('error', function (e) {
+    //   console.log('error: ' + e);
+    // });
+
+    //req.end();
+  });
+
+});
