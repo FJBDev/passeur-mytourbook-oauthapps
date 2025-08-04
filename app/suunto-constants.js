6991c8f916fd0b7bf0ed2e839f3389f54941de38
FJBDev <25235665+FJBDev@users.noreply.github.com>
2025-08-04 17:12:32 -0600

    not needed

--- app/suunto-constants.js
+++ app/suunto-constants.js
@@ -1,6 +1,6 @@
 export const SUUNTO_BASE_URL = "https://cloudapi.suunto.com/v"
-export const SUUNTO_BASE_V2 = SUUNTO_BASE_URL + "2/"
-export const SUUNTO_BASE_V3 =SUUNTO_BASE_URL + "3/"
+export const SUUNTO_BASE_V2 = SUUNTO_BASE_URL + "2"
+export const SUUNTO_BASE_V3 =SUUNTO_BASE_URL + "3"
 
 //TODO FB
 // Segregate the strava endpoints
