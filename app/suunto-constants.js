6141ceb03f93753bd8f8704f61cbbd831e02ca76
FJBDev <25235665+FJBDev@users.noreply.github.com>
2025-08-04 17:12:55 -0600

    dont export

--- app/suunto-constants.js
+++ app/suunto-constants.js
@@ -1,4 +1,4 @@
-export const SUUNTO_BASE_URL = "https://cloudapi.suunto.com/v"
+const SUUNTO_BASE_URL = "https://cloudapi.suunto.com/v"
 export const SUUNTO_BASE_V2 = SUUNTO_BASE_URL + "2"
 export const SUUNTO_BASE_V3 =SUUNTO_BASE_URL + "3"
 
