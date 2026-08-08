05b61e0d4ef0011e8e486f5ea1d0ed8617583dce
FJBDev <25235665+FJBDev@users.noreply.github.com>
2026-08-07 22:02:57 -0600

    typo

--- app/openweathermap3.js
+++ app/openweathermap3.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const xss = require('xss');
 
-function timeMachine(request, response, isTimemachine) {
+function timeMachine(request, response, isTimeMachine) {
 
     if (!request.query.units || xss(request.query.units) !== 'metric') {
         response.status(400).send("Error");
         return;
     }
     var openWeatherMapBaseUrl = 'https://api.openweathermap.org/data/3.0/onecall';
-    if (isTimemachine) {
+    if (isTimeMachine) {
         openWeatherMapBaseUrl += '/timemachine';
     }
     var url = openWeatherMapBaseUrl + '?units=metric&appid=' + process.env.OPENWEATHERMAP_KEY;
