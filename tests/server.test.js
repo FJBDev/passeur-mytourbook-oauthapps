7163224a0573745bcd85e39667556df6081878ba
FJBDev <25235665+FJBDev@users.noreply.github.com>
2026-08-07 21:51:58 -0600

    strava

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,9 +3,6 @@ const supertest = require('supertest');
 const requestWithSupertest = supertest(app);
 let nock = require('nock');
 let sinon = require('sinon');
-let example = nock('https://www.strava.com/api/v3')
-  .post('/strava/token')
-  .reply(201, { foo: 'bar' });
 const { AuthorizationCode } = require('simple-oauth2');
 
 function getRandomArbitrary(min, max) {
@@ -31,12 +28,6 @@ afterAll(() => {
 
 let http = require('http');
 const { assert } = require('console');
-let options = {
-  host: 'example.com',
-  port: 80,
-  path: '/strava/token',
-  method: 'POST'
-}
 
 describe('OpenWeatherMap 3.0 Historical Weather Retrieval', () => {
 
@@ -99,47 +90,6 @@ describe('WeatherApi Weather Retrieval', () => {
     const res = await requestWithSupertest.get('/weatherapi?lat=40.26&lon=-105.58&dt=' + dt);
 
     expect(res.status).toEqual(200);
-  });
-
-});
-
-describe('Strava Token Retrieval', () => {
-
-  test('POST /strava/token should return 201', async () => {
-
-    //expect.hasAssertions();
-    // const spy = sinon.spy(capp, 'retrieveStravaToken');
-    // jest.spyOn(app, 'retrieveStravaToken').mockImplementation(() => {
-    //   return {
-    //     "hikes": [
-    //       {
-    //         "route_id": 123,
-    //         "title": "My Mocked Hike",
-    //         "stats": {
-    //           "startTime": "2019-10-30T16:49:40Z",
-    //           "endTime": "2019-10-30T19:05:28Z"
-    //         }
-    //       }
-    //     ]
-    //   }
-    // });
-    //jwtSpy.mockReturnValue('Some decoded token');
-    const res = await requestWithSupertest.post('/strava/token').send('{ "client_id":"client_id", "refresh_token":"refresh_token", "client_secret":"client_secret", "code":"code", "grant_type":"authorization_code" }');
-    //assert(spy.called);
-    console.log(res);
-    expect(res.status).toEqual(200);
-    // let req = http.request(options, function (res) {
-    //   expect(res.tatus).toEqual(200);
-    //   res.on('data', function (chunk) {
-    //     console.log('BODY: ' + chunk);
-    //   });
-    // });
-
-    // req.on('error', function (e) {
-    //   console.log('error: ' + e);
-    // });
-
-    //req.end();
   });
 
 });
